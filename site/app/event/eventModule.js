e12a759e481ba8971af29ef83ff0509c313032d9
DanielCauser <danielcauser@gmail.com>
2017-03-11 21:02:38 -0300

    First commit

---
+++ site/app/event/eventModule.js
@@ -0,0 +1,7 @@
+﻿//(function () {
+    angular.module('eventModule', [])
+
+        .controller('EventCtrl', ['$scope', function ($scope) {
+            $scope.title = "Test for scope in a controller!"
+        }])
+//})
